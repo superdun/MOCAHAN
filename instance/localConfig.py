bc85abee2d57650c8c4c7d1abda1b79b92ef86e6
LD <475098936@qq.com>
2019-04-15 22:10:31 +0800

    Update localConfig.py

--- instance/localConfig.py
+++ instance/localConfig.py
@@ -1,10 +1,1 @@
 # -*- coding:utf-8 -*-
-
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:password@localhost:3306/mocahan'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = '123456'
-
-QINIU_ACCESS_KEY = 'u1M-QQ-m0ciNAhDn2AZ6iODyKnjUmY7EW1uH2ZiZ'
-QINIU_SECRET_KEY = 'QN3dDZvbX5MdDxfWsf4vua774Wz_5JFCZP78PGTU'
-QINIU_BUCKET_NAME = 'kuma'
-QINIU_BUCKET_DOMAIN = 'pcxbb81qx.bkt.clouddn.com/'
